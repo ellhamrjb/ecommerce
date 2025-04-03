7994f65d420786ddb8650865978ddab781a72795
elham <ellhamrjb@gmail.com>
2025-04-03 17:36:19 +0330

    cart updating

--- cart/cart.py
+++ cart/cart.py
@@ -11,3 +11,11 @@ class Cart():
 
         #make sure cart is available in all pages
         self.cart= self
+    def add(self,product):
+       product_id=str(product.id)
+       if product_id in self.cart:
+          pass
+       else:
+          self.cart[product_id]={'price':str(product.price)}
+
+       self.session.modified=True
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
 
 def cart_add(request):
-    pass
+    cart=Cart(request)
+    if request.POST.get('action')=='post':
+        product_id=int(request.POST.get('product_id'))
+        #search product in db
+        product=get_object_or_404(Product,id=product_id)
+        #save to session
+        Cart.add(product=product)
+
+        response=JsonResponse({'product_name':product.anme})
+
 
 def cart_delete(request):
     pass
